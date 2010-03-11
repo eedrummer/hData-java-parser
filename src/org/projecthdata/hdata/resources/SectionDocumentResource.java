c4a16dca44a8cf5b3b28f56d81a4e0d8da2a07f1
Andy Gregorowicz <andy@mitre.org>
2010-03-11 14:56:28 -0500

    moving over the REST code and dependencies for it

---
+++ src/org/projecthdata/hdata/resources/SectionDocumentResource.java
@@ -0,0 +1,140 @@
+/*
+ *    Copyright 2009 The MITRE Corporation
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+package org.projecthdata.hdata.resources;
+
+import com.sun.jersey.api.core.ResourceConfig;
+import org.projecthdata.hdata.resources.utils.HttpNotFoundException;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
+import org.projecthdata.hdata.hrf.HRF;
+import org.projecthdata.hdata.hrf.Section;
+import org.projecthdata.hdata.hrf.SectionDocument;
+import org.projecthdata.hdata.resources.utils.HDataRestConfig;
+import org.projecthdata.hdata.resources.utils.ModifyHandleHelper;
+
+/**
+ *
+ * @author GBEUCHELT
+ */
+public class SectionDocumentResource extends AbstractResource {
+
+    private final String documentid;
+    private final String sectionPath;
+    private final Section section;
+    private final SectionResource parentResource;
+    private final HRF hrf;
+    @Context
+    private ResourceConfig rc;
+
+
+    public SectionDocumentResource(HRF hrf, String sectionPath, String documentid, SectionResource parentResource) {
+
+        this.hrf = hrf;
+        this.documentid = documentid;
+        this.sectionPath = sectionPath;
+        this.section = hrf.getSection(sectionPath);
+        this.parentResource = parentResource;
+
+    }
+
+    @GET
+    public Response getDocument() {
+
+        SectionDocument doc = section.getDocuments().get(documentid);
+
+        if (doc == null) {
+            throw new HttpNotFoundException("No document with documentid " + documentid +
+                    " in this section found.");
+        }
+
+        try {
+            return Response.ok(((ByteArrayOutputStream) doc.marshall()).toString("UTF-8"), MediaType.APPLICATION_XML_TYPE).build();
+        } catch (Exception ex) {
+            Logger.getLogger(SectionDocumentResource.class.getName()).log(Level.SEVERE, null, ex);
+            return Response.serverError().build();
+        }
+    }
+
+    @DELETE
+    public Response deleteDocument() {
+
+        if (section.removeSectionDocument(documentid) == null) {
+            return Response.serverError().build();
+        }
+        try {
+            notifyChange(hrf);
+        } catch (Exception ex) {
+            Logger.getLogger(SectionDocumentResource.class.getName()).log(Level.SEVERE, null, ex);
+            return Response.serverError().build();
+
+        }
+
+        return Response.status(204).build();
+    }
+
+    @POST
+    public Response illegalOperation() {
+        return Response.status(405).build();
+    }
+
+    @PUT
+    @Consumes({MediaType.APPLICATION_XML})
+    public Response handlePut(@Context UriInfo uriInfo, InputStream data) {
+        Response result = Response.status(Response.Status.BAD_REQUEST).build();
+
+        SectionDocument doc = section.getDocuments().get(documentid);
+
+        if (doc == null) {
+            throw new HttpNotFoundException("No document with documentid " + documentid +
+                    " in this section found.");
+        }
+
+
+        try {
+            result = ModifyHandleHelper.handleDocument(hrf,
+                    HDataRestConfig.getInstance(rc).resolveExtension(section.getUriTypeId().toString()),
+                    doc.getDocumentMetaData(),  // TODO: this must be updated first: date, identity of caller, etc.
+                    data,
+                    section,
+                    uriInfo,
+                    true);
+            notifyChange(hrf);
+        } catch (Exception ex) {
+            Logger.getLogger(SectionDocumentResource.class.getName()).log(Level.SEVERE, null, ex);
+            result = Response.serverError().build();
+        }
+        
+        return result;
+    }
+
+    @Override
+    public void notifyChange(HRF hrf) throws Exception {
+        parentResource.notifyChange(hrf);
+    }
+}
---
+++ src/org/projecthdata/hdata/resources/utils/HDataRestConfig.java
@@ -0,0 +1,113 @@
+/*
+ *    Copyright 2009 The MITRE Corporation
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+package org.projecthdata.hdata.resources.utils;
+
+import com.sun.jersey.api.core.ResourceConfig;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.lang.reflect.Constructor;
+import java.util.Properties;
+import org.projecthdata.hdata.hrf.serialization.HRFFileSystemSerializer;
+import org.projecthdata.hdata.hrf.serialization.HRFSerializer;
+import org.projecthdata.hdata.hrf.util.hDataContentResolver;
+
+/**
+ *
+ * @author GBEUCHELT
+ */
+public class HDataRestConfig {
+
+    private static HDataRestConfig instance;
+    private final HRFSerializer serializer;
+    private final UpdateNotifier notifier;
+
+    private HDataRestConfig(ResourceConfig rc) throws Exception {
+
+        //TODO: the invocation of provided serializers and notifiers should be much more flexible
+
+        String serializerClassName = (String) rc.getProperty("org.projecthdata.hdata.resources.serializer");
+        if (serializerClassName == null) {
+            serializer = new HRFFileSystemSerializer();
+        } else {
+            serializer = (HRFSerializer) java.lang.Class.forName(serializerClassName).newInstance();
+        }
+
+        String notifierClassName = (String) rc.getProperty("org.projecthdata.hdata.resources.notifier");
+        if (notifierClassName == null) {
+            notifier = new HRFFileSystemNotifier(rc);
+        } else {
+            Class[] types = {ResourceConfig.class};
+            Object[] params = {rc};
+
+            Constructor ctr = java.lang.Class.forName(notifierClassName).getConstructor(types);
+            notifier = (UpdateNotifier) ctr.newInstance(params);
+
+        }
+
+        String propertiesFile = "org/projecthdata/hdata/buildingblocks/ccd.properties";
+
+
+        InputStream in = this.getClass().getClassLoader().getResourceAsStream(propertiesFile);
+        if (in == null) {
+            throw new FileNotFoundException();
+        }
+
+        Properties props = new java.util.Properties();
+        props.load(in);
+
+        hDataContentResolver cr = new hDataContentResolver(props);
+
+        cr.registerExtension(serializer);
+
+    }
+
+    public static synchronized HDataRestConfig getInstance(ResourceConfig rc) throws Exception {
+        if (instance == null) {
+            try {
+                instance = new HDataRestConfig(rc);
+            } catch (Exception ex) {
+                if (instance == null) {
+                    throw new Exception("Error initializing configuration object.", ex);
+                }
+            }
+        }
+
+        return instance;
+
+    }
+
+    @Override
+    public Object clone() throws CloneNotSupportedException {
+        throw new CloneNotSupportedException();
+    }
+
+    public HRFSerializer getSerializer() {
+        return serializer;
+    }
+
+    public UpdateNotifier getNotifier() {
+        return notifier;
+    }
+
+    public void registerExtension(String uri, Class clazz) {
+        getSerializer().registerExtension(uri, clazz);
+    }
+
+    public Class resolveExtension(String uri) {
+
+        return getSerializer().resolveExtension(uri);
+    }
+}
