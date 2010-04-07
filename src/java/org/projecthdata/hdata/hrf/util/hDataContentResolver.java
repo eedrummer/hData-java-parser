f40bb8136b0502410d9d6ff8eb4c2d1b41e7ab08
Gerald Beuchelt <beuchelt@bva-172-31-4-24.mitre.org>
2010-04-07 11:23:04 -0400

    Fixed build.xml to include dependencies Added static resolver method to hDataContentResolver

--- src/java/org/projecthdata/hdata/hrf/util/hDataContentResolver.java
+++ src/java/org/projecthdata/hdata/hrf/util/hDataContentResolver.java
@@ -10,6 +10,11 @@ import java.net.URISyntaxException;
 import java.util.HashMap;
 import java.util.Map.Entry;
 import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.JAXBIntrospector;
 import org.projecthdata.hdata.hrf.serialization.HRFSerializer;
 
 /**
@@ -22,12 +27,8 @@ public class hDataContentResolver {
 	private final HashMap<Class, URI> map;
 
     @SuppressWarnings("unchecked")
-	public hDataContentResolver(Properties configuration) throws ClassNotFoundException, URISyntaxException {
+	public hDataContentResolver() {
         map = new HashMap<Class, URI>();
-
-        for (Entry e : configuration.entrySet()) {
-            map.put(Class.forName((String) e.getKey()), new URI((String) e.getValue()));
-        }
     }
 
     @SuppressWarnings("unchecked")
@@ -41,5 +42,30 @@ public class hDataContentResolver {
             fss.registerExtension(e.getValue().toString(), e.getKey());
         }
 
+    }
+
+    public void loadExtensionsFromProperties(Properties configuration) throws ClassNotFoundException, URISyntaxException {
+        for (Entry e : configuration.entrySet()) {
+            map.put(Class.forName((String) e.getKey()), new URI((String) e.getValue()));
+        }
+    }
+
+
+    public static URI resolveClassToUri(Object o) {
+        URI result = null;
+        try {
+            JAXBIntrospector i = JAXBContext.newInstance(o.getClass()).createJAXBIntrospector();
+            Object ob = JAXBIntrospector.getValue(o);
+
+            result = new URI(i.getElementName(ob).getNamespaceURI());
+
+        } catch (URISyntaxException ex) {
+            Logger.getLogger(hDataContentResolver.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (JAXBException ex) {
+            Logger.getLogger(hDataContentResolver.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (NullPointerException ex) {
+            Logger.getLogger(hDataContentResolver.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return result;
     }
 }
